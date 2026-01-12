42ab0475ec046d75d370a251d36b5bcde9d7b8fd
Neha YoruNina <836193631@qq.com>
2026-01-12 20:50:37 +0800

    任务支持类型检测

--- src/main/java/net/tigereye/chestcavity/compat/ftb/ChestCavityQuestEventHandler.java
+++ src/main/java/net/tigereye/chestcavity/compat/ftb/ChestCavityQuestEventHandler.java
@@ -6,12 +6,16 @@ import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
 import dev.ftb.mods.ftbquests.quest.TeamData;
 import dev.ftb.mods.ftbteams.api.Team;
 import dev.ftb.mods.ftbteams.data.TeamManagerImpl;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.tigereye.chestcavity.ChestCavity;
+import net.tigereye.chestcavity.chestcavities.instance.ChestCavityInstance;
+import net.tigereye.chestcavity.interfaces.ChestCavityEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ChestCavityQuestEventHandler {
     private List<OpenChestCavityTask> chestCavityOpenedTaskList = new ArrayList<>();
@@ -37,9 +41,16 @@ public class ChestCavityQuestEventHandler {
             Team team = TeamManagerImpl.INSTANCE.getTeamForPlayer(player).orElse(null);
             if (team == null) return;
             TeamData teamData = ServerQuestFile.INSTANCE.getOrCreateTeamData(team);
+
+            Optional<ChestCavityEntity> optional = ChestCavityEntity.of(entity);
+            if (optional.isEmpty()) return;
+            ChestCavityEntity chestCavityEntity = optional.get();
+            ChestCavityInstance cc = chestCavityEntity.getChestCavityInstance();
+            ResourceLocation inventoryType = cc.getInventoryType();
+
             for (OpenChestCavityTask task : this.chestCavityOpenedTaskList) {
                 if (teamData.getProgress(task) < task.getMaxProgress() && teamData.canStartTasks(task.getQuest())) {
-                    task.chestCavityOpened(teamData, entity);
+                    task.chestCavityOpened(teamData, entity, inventoryType);
                 }
             }
         } catch (Exception e) {
--- src/main/java/net/tigereye/chestcavity/compat/ftb/OpenChestCavityTask.java
+++ src/main/java/net/tigereye/chestcavity/compat/ftb/OpenChestCavityTask.java
@@ -27,17 +27,21 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.ArrayList;
 import java.util.List;
 
+import static net.tigereye.chestcavity.chestcavities.json.ccInvType.InventoryTypeManager.GeneratedInventoryTypeData;
+
 
 public class OpenChestCavityTask extends Task {
     private static final ResourceLocation PLAYER = new ResourceLocation("minecraft:player");
+    private static final ResourceLocation EMPTY = new ResourceLocation("minecraft:empty");
     public long value;
     public ResourceLocation entity;
-
+    public ResourceLocation inventoryType;
 
 
     public OpenChestCavityTask(long id, Quest quest) {
         super(id, quest);
-        this.entity = PLAYER;
+        this.entity = EMPTY;
+        this.inventoryType = EMPTY;
         this.value = 1L;
     }
 
@@ -56,6 +60,7 @@ public class OpenChestCavityTask extends Task {
         super.writeData(nbt);
         nbt.putLong("value", value);
         nbt.putString("entity", entity.toString());
+        nbt.putString("inventoryType", inventoryType.toString());
     }
 
     @Override
@@ -66,6 +71,7 @@ public class OpenChestCavityTask extends Task {
             this.value = 1;
         }
         this.entity = new ResourceLocation(nbt.getString("entity"));
+        this.inventoryType = new ResourceLocation(nbt.getString("inventoryType"));
     }
 
     @Override
@@ -73,6 +79,7 @@ public class OpenChestCavityTask extends Task {
         super.writeNetData(buffer);
         buffer.writeLong(this.value);
         buffer.writeResourceLocation(this.entity);
+        buffer.writeResourceLocation(this.inventoryType);
     }
 
     @Override
@@ -80,6 +87,7 @@ public class OpenChestCavityTask extends Task {
         super.readNetData(buffer);
         this.value = buffer.readLong();
         this.entity = buffer.readResourceLocation();
+        this.inventoryType = buffer.readResourceLocation();
     }
 
     @Override
@@ -87,15 +95,23 @@ public class OpenChestCavityTask extends Task {
     public void fillConfigGroup(ConfigGroup config) {
         super.fillConfigGroup(config);
         List<ResourceLocation> ids = new ArrayList<>(ForgeRegistries.ENTITY_TYPES.getKeys());
+        ids.add(EMPTY);
         config.addEnum("entity", this.entity, (v) -> {
             this.entity = v;
-        }, NameMap.of(PLAYER, ids).nameKey((v) -> {
+        }, NameMap.of(EMPTY, ids).nameKey((v) -> {
             String namespace = v.getNamespace();
             return "entity." + namespace + "." + v.getPath();
         }).icon((v) -> {
             SpawnEggItem item = ForgeSpawnEggItem.fromEntityType(ForgeRegistries.ENTITY_TYPES.getValue(v));
             return ItemIcon.getItemIcon(item != null ? item : Items.SPAWNER);
-        }).create(), PLAYER);
+        }).create(), EMPTY);
+
+        List<ResourceLocation> invIds = new ArrayList<>(GeneratedInventoryTypeData.keySet());
+        invIds.add(EMPTY);
+        config.addEnum("inventoryType", this.inventoryType, (v) -> {
+            this.inventoryType = v;
+        }, NameMap.of(EMPTY, invIds).name((v) -> Component.literal(v.getPath())).create(), EMPTY);
+
         config.addLong("value", this.value, (v) -> {
             this.value = v;
         }, 1L, 1L, Long.MAX_VALUE);
@@ -120,10 +136,19 @@ public class OpenChestCavityTask extends Task {
     public void onButtonClicked(Button button, boolean canClick) {
     }
 
-    public void chestCavityOpened(TeamData teamData, LivingEntity entity) {
-        if (!teamData.isCompleted(this) && this.entity.equals(RegistrarManager.getId(entity.getType(), Registries.ENTITY_TYPE))) {
+    public void chestCavityOpened(TeamData teamData, LivingEntity entity, ResourceLocation inventoryType) {
+        if (teamData.isCompleted(this)) return;
+        boolean isEntityEmpty = this.entity.equals(EMPTY);
+        boolean isInvTypeEmpty = this.inventoryType.equals(EMPTY);
+
+        if (!isEntityEmpty) {
+            if (this.entity.equals(RegistrarManager.getId(entity.getType(), Registries.ENTITY_TYPE))) {
+                if (isInvTypeEmpty || this.inventoryType.equals(inventoryType)) {
+                    teamData.addProgress(this, 1L);
+                }
+            }
+        } else if (!isInvTypeEmpty && this.inventoryType.equals(inventoryType)) {
             teamData.addProgress(this, 1L);
         }
     }
-
 }
